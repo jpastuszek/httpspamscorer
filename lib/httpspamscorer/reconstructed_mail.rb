b01fe6e05a6db7c87768222b093390ef7ca288d7
Jakub Pastuszek <jpastuszek@gmail.com>
2014-09-23 12:26:40 +0100

    more work on e-mail reconstruction

--- lib/httpspamscorer/reconstructed_mail.rb
+++ lib/httpspamscorer/reconstructed_mail.rb
@@ -1,23 +1,27 @@
 require 'mail'
 
 class ReconstructedMail < Mail::Message
-	def initialize(_recipient, _sender, _from, _subject, _body)
-		super() do
-			to      _recipient
-			from    _from
-			subject _subject
-			body    _body
+	def initialize(_headers, _text_body, _html_body, _attachements)
+		super()
+
+		self.headers Hash[_headers]
+
+		self.text_part = Mail::Part.new do
+			body _text_body
+		end
+
+		self.html_part = Mail::Part.new do
+			content_type 'text/html; charset=UTF-8'
+			body _html_body
 		end
 	end
 
-	def from_hash(msg)
-		p msg
+	def self.from_hash(msg)
 		self.new(
-			msg['recipient'],
-			msg['sender'],
-			msg['from'],
-			msg['subject'],
-			msg['body-plain']
+			msg['message-headers'],
+			msg['body-plain'],
+			msg['body-html'],
+			msg['attachements']
 		)
 	end
 end
--- spec/reconstructed_mail_spec.rb
+++ spec/reconstructed_mail_spec.rb
@@ -3,14 +3,34 @@ require_relative 'spec_helper'
 require_relative '../lib/httpspamscorer/reconstructed_mail'
 
 describe ReconstructedMail do
+	let :spam do
+		Mail.read('spec/support/spam1.eml')
+	end
+
 	describe '#from_hash' do
 		it 'should create e-mail message object from hash of values' do
+			headers = spam.header.to_a.map{|h| [h.name, h.value]}
+
+			#puts spam.attachments
+
 			msg = described_class.from_hash(
-				'from' => 'foo@baz.com'
+				'body-plain' => spam.text_part.body.to_s,
+				'body-html' => spam.html_part.body.to_s,
+				'message-headers' => headers
+				#'attachements' =>
 			)
 
-			p msg
-			puts msg.to_s
+			#p msg
+			#puts msg.to_s
+
+			expect(msg.text_part.body.to_s).to eq(spam.text_part.body.to_s)
+			expect(msg.html_part.body.to_s).to eq(spam.html_part.body.to_s)
+
+			msg.header.to_a.map{|h| [h.name, h.value]}
+			.reject{|name, _| name == 'Content-Type'} # boundry will differ
+			.each do |h|
+				expect(headers).to include(h)
+			end
 		end
 	end
 end
